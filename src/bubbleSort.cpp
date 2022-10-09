784cd72ca0d9ada6f25f882bba47a8fab8fdbd7c
Zofia Lenarczyk <zosia.lenarczyk1110@gmail.com>
2022-10-09 17:31:22 +0200

    bubblesort using std::vector

--- src/bubbleSort.cpp
+++ src/bubbleSort.cpp
@@ -4,18 +4,45 @@
 
 #include "structure.hpp"
 
-void BubbleSort::sort(int arr[]) {
+void BubbleSort::sort(std::vector<int> &arr) {
+    /*
+     * Iterates through an array and checks every two neighbouring elements. If an element with a higher
+     * index is smaller then elements get swapped.
+     * Goes until no swap is needed.
+     */
 
-    int n = sizeof(arr) / sizeof(arr[0]);
+    std::size_t n = arr.size();
 
     while (n > 1) {
-        int new_n = 0;
-        for (int i = 1; i < n - 1; ++i){
-            if (arr[i - 1] > arr[i]){
+        std::size_t new_n = 0;
+        // n - 1 because length of the array does not equal the index of the last element
+        for(std::size_t i = 1; i < n - 1; ++i){
+            if( arr[i - 1] > arr[i]){
                 arr[i - 1], arr[i] = arr[i], arr[i - 1];
-                new_n = i;
+                new_n = i; // new_n is the number of index of the swapped element
             }
         }
-        n = new_n;
+        n = new_n; // Here, new_n is the number of index of the last swapped element.
+                    // It optimizes the algorithm, because the rest of the array, that is in order,
+                    // will not be checked again.
     }
 }
+
+// bubblesort implementaion with raw arrays - decided to do it using std::vector instead
+// beacuse of object-oriented structure
+
+//void BubbleSort::sort(int arr[]) {
+//
+//    int n = sizeof(arr) / sizeof(arr[0]);
+//
+//    while (n > 1) {
+//        int new_n = 0;
+//        for (int i = 1; i < n - 1; ++i){
+//            if (arr[i - 1] > arr[i]){
+//                arr[i - 1], arr[i] = arr[i], arr[i - 1];
+//                new_n = i;
+//            }
+//        }
+//        n = new_n;
+//    }
+//}
